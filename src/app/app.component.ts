a5f79e6e266c924bf0ee922c6ebc6bb565b1328e
Tabalamari <tabalamari@gmail.com>
2026-02-05 18:17:46 +0200

    Add file input with validation and image preview
    Implemented file selection, validation (type, size), and image preview.
    Error messages are shown for invalid files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,53 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'image-to-text-converter';
+    title = 'Image to Text Converter';
+
+    selectedFile: File | null = null;
+    previewUrl: string | null = null;
+    errorMessage: string | null = null;
+
+    onFileSelected(event: Event): void {
+        const input = event.target as HTMLInputElement;
+
+        if (!input.files || input.files.length === 0) {
+            this.selectedFile = null;
+            this.previewUrl = null;
+            return;
+        }
+        const file = input.files[0];
+        if (!file.type.startsWith('image/')) {
+            this.errorMessage = 'Please choose the file.';
+            this.selectedFile = null;
+            this.previewUrl = null;
+            return;
+        }
+        if (!(file.type === 'image/jpeg' || file.type === 'image/png')) {
+            this.errorMessage = 'Allowed formats: JPEG or PNG.';
+            this.selectedFile = null;
+            this.previewUrl = null;
+            return;
+        }
+
+        if (file.size > 2 * 1024 * 1024) {
+            this.errorMessage = 'File must be less than 2MB.';
+            this.selectedFile = null;
+            this.previewUrl = null;
+            return;
+        }
+        this.selectedFile = file;
+        this.errorMessage = null;
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.previewUrl = reader.result as string;
+        };
+        reader.readAsDataURL(file);
+    }
 }
